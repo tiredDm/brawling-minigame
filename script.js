d7c6bb2c5d29162bae8a4cf9038db9979cea7223
tiredDm <ronpri1@gmail.com>
2023-10-26 18:57:18 -0400

    Began Logic

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const container = document.querySelector('.container');
 
 let gameState = 0; //0 means no one has submitted, 1 means one person has, 2 means both has, and 3 means game over..
 
+let lCurrentAction;
+let rCurrentAction;
+
 function addWin(points){
     //Have a version for Right and Left..
     winCounters[0].innerHTML = points;
@@ -24,6 +27,15 @@ rightSubmit.addEventListener('click', () => {
 });
 
 function processSubmit(submitter){
+    //Collect all Document Elements
+    let r_roll1 = document.querySelector('#r_roll_one');
+    let r_roll2 = document.querySelector('#r_roll_two');
+    let r_roll3 = document.querySelector('#r_roll_three');
+    let l_roll1 = document.querySelector('#roll_one');
+    let l_roll2 = document.querySelector('#roll_two');
+    let l_roll3 = document.querySelector('#roll_three');
+
+
     if(submitter.classList.contains('r-submit-button')) {
         console.log('Right button Pressed');
         //Creat Game Wait Element..
@@ -31,6 +43,9 @@ function processSubmit(submitter){
         waitScreenRight.classList.add('waitScreenRight');
         waitScreenRight.innerHTML='Please Wait for the Other Player to Finish';
         container.appendChild(waitScreenRight);
+        
+        console.log(r_roll1.value, r_roll2.value, r_roll3.value);
+
     } else {
         console.log('Left button Pressed');    
         //Creat Game Wait Element..
@@ -38,16 +53,29 @@ function processSubmit(submitter){
         waitScreenLeft.classList.add('waitScreenLeft');
         waitScreenLeft.innerHTML='Please Wait for the Other Player to Finish';
         container.appendChild(waitScreenLeft);    
+        console.log(l_roll1.value, l_roll2.value, l_roll3.value);
     }
     gameState += 1;
     if(gameState % 2 == 0){
         evaluateRound();
+        //CLear all Roll Areas
+        r_roll1.value='';
+        r_roll2.value='';
+        r_roll3.value='';
+        l_roll1.value='';
+        l_roll2.value='';
+        l_roll3.value='';
     }
 
 }
 
-function evaluateRound(){
+function evaluateRound(){ //input array of values... then for each value do shit.. have the six roll as parameters.. then create a loop over each.. in which you order divs made and inputs..
     console.log('Pizza Time');
+
+
+
+
+    /* ADDING THE POPUP */
     let endOfRoundScreen = document.createElement('div');
     endOfRoundScreen.classList.add('endOfRound');
     
@@ -57,6 +85,12 @@ function evaluateRound(){
     endRoundButton.addEventListener('click', () => {
         finalizeRound();
     });
+
+    //Populate End Of Round POPUP..
+    let div = document.createElement('div');
+    div.innerHTML = '0';
+
+    endOfRoundScreen.appendChild(div);
     endOfRoundScreen.appendChild(endRoundButton)
     container.appendChild(endOfRoundScreen);
 }
@@ -74,3 +108,4 @@ function finalizeRound(){
 function endGame(){
     console.log('Game Over Bro')
 }
+
