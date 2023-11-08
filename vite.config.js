c7d9fb86d2ab5fbc5f729cde285f8c8eacfad0de
tiredDm <ronpri1@gmail.com>
2023-11-08 13:02:51 -0500

    Just adding node module updates

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/brawling-minigame/',
-  build: {
-    chunkSizeWarningLimit: 1600,
-  },
+ 
 })
